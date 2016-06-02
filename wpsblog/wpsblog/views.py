2d1a6a1f0559e6c854649c8b82ceff91db0a1ddb
wonju <gahuins@gmail.com>
2016-06-02 23:35:06 +0900

    init news view:render watcha news as html

--- wpsblog/wpsblog/views.py
+++ wpsblog/wpsblog/views.py
@@ -1,3 +1,4 @@
+import json
 import requests
 
 from django.http.response import HttpResponse
@@ -13,4 +14,27 @@ def room(request, room_id):
     return HttpResponse(
 	    response.content,
 		content_type="application/json",
+	)
+
+
+def news(request):
+    search = request.GET.get("search")
+
+    response = requests.get("https://watcha.net/home/news.json?page=1&per=50")
+    news_dict = json.loads(response.text)
+
+    content = "<h1>News</h1>" +\
+	    "<p>This is news page</p>" +\
+		"".join([
+		    "<h2>{title}</h2><img src={image_src}><p>{content}</p>".format(
+			    title=news.get('title'),
+				image_src=news.get('image'),
+				content=news.get('content'),
+			)
+			for news
+			in news_dict["news"]
+		])
+
+    return HttpResponse(
+	    content,
 	)
