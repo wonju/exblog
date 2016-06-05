41b4b6cac2cb1a1d6539df55802763c50de2314b
wonju <gahuins@gmail.com>
2016-06-06 03:39:47 +0900

    init pep8 costom settings: max length to 119

--- wpsblog/wpsblog/urls/__init__.py
+++ wpsblog/wpsblog/urls/__init__.py
@@ -7,10 +7,10 @@ from wpsblog.views import *
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
-	url(r'^$', home, name="home"),
+    url(r'^$', home, name="home"),
     url(r'^about/us/$', about, name="about"),
-	url(r'^rooms/(?P<room_id>\d+)/$', room, name="room"),
-	url(r'^news/$', news, name="news"),
+    url(r'^rooms/(?P<room_id>\d+)/$', room, name="room"),
+    url(r'^news/$', news, name="news"),
 
     url(r'^policy/', include("wpsblog.urls.policy", namespace="policy")),
 ]
