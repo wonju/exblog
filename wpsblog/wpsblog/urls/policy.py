6f4e5a29b5d87e26a8512037bc265a59ef4caa1a
wonju <gahuins@gmail.com>
2016-06-06 02:08:47 +0900

    add django-debug-toolbar

--- wpsblog/wpsblog/urls/policy.py
+++ wpsblog/wpsblog/urls/policy.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 
 from wpsblog.views import *
 
-policy_urlpatterns = [
+urlpatterns = [
     url(r'/terms/$', terms, name="terms"),
     url(r'/privacy/$', privacy, name="privacy"),
     url(r'/disclaimer/$', disclaimer, name="disclaimer"),
