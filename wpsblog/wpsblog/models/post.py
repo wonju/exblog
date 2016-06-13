0a1b8ba6c3ea9beab5bea89ffb2b4aabb234ee4c
wonju <gahuins@gmail.com>
2016-06-13 19:10:59 +0900

    refactor form, models.py: seperate post, naver_post

--- wpsblog/wpsblog/models/post.py
+++ wpsblog/wpsblog/models/post.py
@@ -13,24 +13,16 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse(
-            "post:detail",
+            "posts:detail",
             kwargs={
                 "post_id": self.id,
             }
         )
 
-
-class NaverPost(models.Model):
-
-    keyword = models.CharField(
-        max_length=16,
-    )
-    title = models.CharField(
-         max_length=256
-    )
-    content = models.TextField()
-    thumbnail_image_url = models.URLField()
-    original_url = models.URLField()
-
-    def __str__(self):
-        return self.title
+    def get_update_url(self):
+        return reverse(
+            "posts:update",
+            kwargs={
+                "post_id": self.id,
+            }
+        )
